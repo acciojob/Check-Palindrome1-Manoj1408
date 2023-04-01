a1cea849c4ae338b07e4bd605ce7acac576c811c
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-01 11:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 
 function palindrome(str){
 
+	str.replaceAll(" ","");
+	start = 0;
+	end = str.length-1;
+	while(start <end){
+		if(str.charAt(start) != str.charAt(end)){
+			return false;
+		}
+		start++;
+		end--;
+	}
+	return true;
 }
 module.exports = palindrome
